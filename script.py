d5bc8f8c22f3280f01bb71587a207fcddad75e61
Leonard <leonard.sellem@gmail.com>
2019-04-04 12:17:22 +0200

    add cheapest_shipping function

--- script.py
+++ script.py
@@ -28,3 +28,18 @@ def ds_cost(weight):
 
 #Test ds_cost
 #print(ds_cost(1.5))
+
+#Cheapest method function
+def cheapest_shipping(weight):
+    if (gs_cost(weight) < ds_cost(weight)) and (gs_cost(weight) < pgs_cost):
+        print("The cheapest shipping method is Ground Shipping. It will cost you $"+str(gs_cost(weight)))
+    elif (ds_cost(weight) < gs_cost(weight)) and (ds_cost(weight) < pgs_cost):
+        print("The cheapest shipping method is Drone Shipping. It will cost you $"+str(ds_cost(weight)))
+    elif (pgs_cost < ds_cost(weight)) and (pgs_cost < gs_cost(weight)):
+        print("The cheapest shipping method is Premium Ground Shipping. It will cost you $"+str(pgs_cost))
+    elif (gs_cost(weight) < ds_cost(weight)) and (gs_cost(weight) == pgs_cost):
+        print("You can either choose Ground or Premium Ground Shipping. In both case, it will cost you $" + str(pgs_cost))
+    elif (ds_cost(weight) < gs_cost(weight)) and (ds_cost(weight) == pgs_cost):
+        print("You can either choose Drone or Premium Ground Shipping. In both case, it will cost you $" + str(pgs_cost))
+    else:
+        print("You can either choose Ground  or Drone Shipping. In both case, it will cost you $" + str(ds_cost(weight)))
