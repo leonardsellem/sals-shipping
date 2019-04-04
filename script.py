d7a949c35599c7689ceda0458a049c13a6f97648
Leonard <leonard.sellem@gmail.com>
2019-04-04 11:55:04 +0200

    add ds_cost function + tests

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+# Ground shipping function
 def gs_cost(weight):
   if weight <= 2.0:
     return 20.0 + 1.5 * weight
@@ -8,4 +9,22 @@ def gs_cost(weight):
   else:
     return 20.0 + 4.75 * weight
   
-print(gs_cost(8.4))
+#Test gs_cost
+#print(gs_cost(8.4))
+
+#Premium ground shipping variable (fixed cost)
+pgs_cost = 125.0
+
+# Drone shipping function
+def ds_cost(weight):
+  if weight <= 2.0:
+    return 4.5 * weight
+  elif weight <= 6.0:
+    return 9.0 * weight
+  elif weight <= 10.0:
+    return 12.0 * weight
+  else:
+    return 14.25 * weight
+
+#Test ds_cost
+#print(ds_cost(1.5))
